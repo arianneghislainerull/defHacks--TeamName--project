b8e99dbaa34c16b67d021f6b63cee61053aaa3bf
ruai0511 <67210127+ruai0511@users.noreply.github.com>
2020-06-21 20:42:24 +0800

    Add files via upload

---
+++ emotionSelector2.py
@@ -0,0 +1,31 @@
+import random
+from PIL import Image
+from pathlib import Path
+from playsound import playsound
+
+def thepath(emotion):
+    #randomly choose a path of the given expression
+    list = ["./Faces/"+emotion+".jpg", "./Faces/"+emotion+"2.jpg", "./Faces/"+emotion+"3.jpg"]
+    chosenpath = random.choice(list)
+    return chosenpath
+
+
+def soundpath(emotion):
+    return "./Sounds/"+emotion+".mp3"
+
+
+#randomly select a picture of the given emotion and paly the sound
+#emotion is a string eg. reaction("happy")
+def reaction(emotion):
+    image = Image.open(Path(thepath(emotion)))
+    #resize Image
+    image = image.resize((512,512))
+    image.show()
+    playsound(soundpath(emotion))
+    return image
+
+def randomEmotion():
+    #choose randomly from all emotions
+    list = ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"]
+    emotion = random.choice(list)
+    return reaction(emotion)
