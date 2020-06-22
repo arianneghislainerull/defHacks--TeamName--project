23124ccfd76e2770312dd2bb8a33b337b4e7d3eb
katbasie <58278164+katbasie@users.noreply.github.com>
2020-06-21 18:38:42 -0700

    cosmetic changes

--- mainApp.py
+++ mainApp.py
@@ -2,14 +2,17 @@ import tkinter as tk
 import subprocess, os
 from tkinter import font  as tkfont # python 3
 
+from PIL import ImageTk, Image
+
 
 class SampleApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
         self.winfo_toplevel().title("TherAssist")
+
+
         # the container is where we'll stack a bunch of frames on top of each other, then the one we want visible
         # will be raised above the others
         container = tk.Frame(self)
@@ -21,6 +24,7 @@ class SampleApp(tk.Tk):
         for F in (StartPage, PageOne, PageTwo):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
+            frame.configure(bg='white')
             self.frames[page_name] = frame
 
             # put all of the pages in the same location;
@@ -41,24 +45,41 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="Face Detection", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
 
+        # Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+        global tImg
+        tImg = ImageTk.PhotoImage(Image.open('logo3.png'))
+
+        # The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+        panel = tk.Label(self, image=tImg)
+
+        # The Pack geometry manager packs widgets in rows or columns.
+        panel.pack(side="top", fill="both", expand="no")
+
+
+
+        global img,img2,img3
+        img = tk.PhotoImage(file="buttonManual.png")  # make sure to add "/" not "\"
+        img2 = tk.PhotoImage(file="btnHW.png")  # make sure to add "/" not "\"
+        img3 = tk.PhotoImage(file="btnEmo.png")  # make sure to add "/" not "\"
         button1 = tk.Button(self, text="Manual",
-                            command=lambda: controller.show_frame("PageOne"))
+                            command=lambda: controller.show_frame("PageOne"), image = img)
+        button1.image = img
 
         def openHw():
             controller.show_frame("PageTwo")
             os.system('emotionSelector.py')
 
         button2 = tk.Button(self, text="Homework",
-                            command=lambda: openHw())
+                            command=lambda: openHw(), image = img2)
+        button2.image = img2
 
         def openEmo():
             controller.show_frame("PageTwo")
             os.system('emotionSelecter.py')
 
-        button3 = tk.Button(self, text="Emotion Selector", command=lambda: openEmo())
+        button3 = tk.Button(self, text="Emotion Selector", command=lambda: openEmo(),image = img3)
+        button3.image = img3
         button1.pack()
         button2.pack()
         button3.pack()
@@ -71,6 +92,7 @@ class PageOne(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="Manual Face Detection", font=controller.title_font)
+        label.configure(bg ='white')
         label.pack(side="top", fill="x", pady=10)
 
         self.input = 0
@@ -90,11 +112,16 @@ class PageOne(tk.Frame):
             result.pack()
             return
 
-        good = tk.Button(self, text = "Satisfactory", command = lambda: changeInput(True))
-        improve = tk.Button(self, text = "Needs Improvement", command = lambda: changeInput(False))
-        finish = tk.Button(self, text = "Finish Session", command = lambda: generateReport())
+        global imgS, imgN, imgF, imgH
+        imgS = tk.PhotoImage(file="button_satisfactory.png")
+        imgN = tk.PhotoImage(file="button_needs-improvement.png")
+        imgF = tk.PhotoImage(file="button_finish-session.png")
+        imgH = tk.PhotoImage(file="button_home.png")
+        good = tk.Button(self, text = "Satisfactory", command = lambda: changeInput(True),image = imgS)
+        improve = tk.Button(self, text = "Needs Improvement", command = lambda: changeInput(False), image = imgN)
+        finish = tk.Button(self, text = "Finish Session", command = lambda: generateReport(), image = imgF)
         button = tk.Button(self, text="Home",
-                           command=lambda: controller.show_frame("StartPage"))
+                           command=lambda: controller.show_frame("StartPage"), image = imgH)
         good.pack()
         improve.pack()
         finish.pack()
@@ -106,10 +133,10 @@ class PageTwo(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="Homework Exercises", font=controller.title_font)
+        label = tk.Label(self, text="Homework Exercises", font=controller.title_font, bg = 'white')
         label.pack(side="top", fill="x", pady=10)
         button = tk.Button(self, text="Home",
-                           command=lambda: controller.show_frame("StartPage"))
+                           command=lambda: controller.show_frame("StartPage"), image = imgH)
         button.pack()
 
 
